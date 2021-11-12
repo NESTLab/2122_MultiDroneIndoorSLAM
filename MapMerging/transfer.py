b86bc96ba7ea7bb47005a6492b6617fd51b9a518
Tyler Ferrara <tylerferrara123@gmail.com>
2021-11-12 07:55:42 -0500

    Added type hinting to all classes

--- MapMerging/transfer.py
+++ MapMerging/transfer.py
@@ -1,3 +1,4 @@
+from __future__ import annotations # Allows classes to type hint their own class.
 import numpy as np
 import hough_sift_mapmerge as merge
 import time, socket, threading
@@ -9,32 +10,32 @@ PORT = 9090
 IP_ADDRESS = "127.0.0.1"
 
 class Map():
-    def __init__(self, map):
+    def __init__(self, map) -> None:
         self.map = map
         self.num_rows = len(map)
         self.num_col = len(map[0])
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.map)
     
-    def to_bytes(self):
+    def to_bytes(self) -> bytes:
         return self.map.tobytes()
     
-    def from_bytes(self, data):
+    def from_bytes(self, data) -> Map:
         m = np.frombuffer(data, dtype=np.uint8)
         m = m.reshape(self.num_rows, self.num_col)
         return Map(m)
 
 # ************* UNUSED
 class Network:
-    def __init__(self, latency, bandwidth):
+    def __init__(self, latency, bandwidth) -> None:
         self._latency = latency
         self._bandwidth = bandwidth
         self._bytesSent = 0
         self._timeCreated = time.time()
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, bytes):
+    def send(self, bytes) -> int:
         now = time.time()
         connectionDuration = now - self._timeCreated
         self._bytesSent += len(bytes)
@@ -44,25 +45,24 @@ class Network:
         time.sleep(max(requiredDuration - connectionDuration, self._latency))
         return self._socket.send(bytes)
     
-    def connect(self, address):
+    def connect(self, address) -> None:
         self._socket.connect(address)
-        print("connected")
     
-    def recv(self, bytes):
+    def recv(self, bytes) -> None:
         self._socket.recv(bytes)
 
 class Robot:
     """Simulated UAV robot"""
-    def __init__(self, name, map):
+    def __init__(self, name, map) -> None:
         self.name = name
         self.map = map
     
-    def send_map(self, sock):
+    def send_map(self, sock) -> None:
         con, _ = sock.accept()
         con.sendall(self.map.to_bytes())
         con.close()
 
-    def recv_map(self, sock):
+    def recv_map(self, sock) -> Map:
         sock.connect((IP_ADDRESS, PORT))
         payload = b''
         while True:
@@ -72,16 +72,15 @@ class Robot:
             else:
                 break
         m = self.map.from_bytes(payload)
-        print("Robot", self.name, "got", m)
 
 class Strategy:
     """Strategy for transfering occupancy grid between robots"""
-    def __init__(self, name, r1, r2):
+    def __init__(self, name, r1, r2) -> None:
         self.name = name
         self.robots = [r1, r2]
     
     # Simulate data transfer from robot1 -> robot2
-    def transfer(self, r1, r2):
+    def transfer(self, r1, r2) -> None:
         # Server
         sock1 = socket.socket()
         sock1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -103,15 +102,33 @@ class Strategy:
         sock2.close()
 
     # Syncronize the maps of both robots
-    def sync(self):
+    def sync(self) -> None:
         r1, r2 = self.robots[0], self.robots[1]
-        print("syncing robots", r1.name, "and", r2.name, "...")
         self.transfer(r1, r2)
         self.transfer(r2, r1)
 
-# Display transfer time, merge accuracy, and bytes transmitted.
-def evaluate(strat):
-    print("evaluating")
+class Evalutation():
+    def __init__(self, strat) -> None:
+        self.strat = strat
+        self.duration = 0 # ms
+        self.megabytes_transfered = 0
+        self.accuracy = 0
+    
+    def start(self) -> Evalutation:
+        start = time.time()
+        strat.sync()
+        end = time.time()
+        self.duration = (end - start) / 1000
+        return self
+
+    def __repr__(self) -> str:
+        return """###############################################
+        Evaluation of "{}"
+
+Duration: \t\t{} ms
+Data Transfered: \t{} Mb
+Accuracy: \t\t{}
+""".format(self.strat.name, self.duration, self.megabytes_transfered, self.accuracy)
 
 if __name__ == "__main__":
     m1, m2 = merge.TRAIN_FILENAMES[0], merge.TRAIN_FILENAMES[1]
@@ -119,7 +136,10 @@ if __name__ == "__main__":
     rob_a = Robot('A', Map(merge.load_mercer_map(m1)))
     rob_b = Robot('B', Map(merge.load_mercer_map(m2)))
     strat = Strategy("testing", rob_a, rob_b)
-    strat.sync()
+
+    e = Evalutation(strat).start()
+    print(e)
+    
 
 """ TODO
 - Add metrics in evalutation function
