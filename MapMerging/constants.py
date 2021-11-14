11978e5b615d6941d111a8232d86b3311f17b3fa
Connor Mclaughlin <cjmclaughlin@wpi.edu>
2021-11-14 03:57:15 -0500

    refactor mapmerge and start real map sim

---
+++ MapMerging/constants.py
@@ -0,0 +1,16 @@
+# map values (float)
+OCCUPIED_FLOAT = 0
+FREE_FLOAT = 1
+UNKNOWN_FLOAT = 0.5
+# map values (uint8)
+FREE = 255
+OCCUPIED = 0
+UNKNOWN = 127
+
+# data paths
+TRAIN_FILENAMES = ["robot0_cast.txt", "robot1_cast.txt", "robot2_cast.txt", "robot3_cast.txt", "LongRun6.txt", "LongRun7.txt", "LongCorridor2.txt", "LongCorridor1.txt"]
+TEST_FILENAMES = ["intel.txt", "intel1000.txt"]
+BASE_MERCER_MAP_PATH = "Data/Mercer/"
+BASE_PGM_MAP_PATH = "Data/PGM/"
+
+PGM_FILENAMES = ["map0.pgm", "map1.pgm", "map2.pgm"]
