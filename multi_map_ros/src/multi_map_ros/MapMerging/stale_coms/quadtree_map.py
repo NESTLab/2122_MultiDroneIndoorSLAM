6e19449c9eda7ad9cfe604819a7ee5b0fc410df3
Connor Mclaughlin <cjmclaughlin@wpi.edu>
2021-11-22 13:57:07 -0500

    fix stack append order bug, passing tests for variety of map sizes

--- multi_map_ros/src/multi_map_ros/MapMerging/stale_coms/quadtree_map.py
+++ multi_map_ros/src/multi_map_ros/MapMerging/stale_coms/quadtree_map.py
@@ -41,6 +41,7 @@ class QuadTreeMap(Map):
             2 -- current quadrant is all unknown
             3 -- current quadrant is all occupied
         Uses first 8 bytes to transfer map size.
+        Uses one extra byte to indicate if array has top/bottom padding (if size is odd)
         """
         def split(arr):
             """
@@ -53,7 +54,7 @@ class QuadTreeMap(Map):
             q4 = arr[mid_x::, mid_y::]
             return [q1, q2, q3, q4]
         
-        def size_to_bytes(size, num_bytes_dim=4):
+        def encode_size(size, num_bytes_dim=4):
             """
             Not sure if we need this or we can assume fixed size
             Although this makes things more flexible for the SLAM team
@@ -76,22 +77,44 @@ class QuadTreeMap(Map):
             while len(y_bytes) < num_bytes_dim:
                 y_bytes.append(0)
             return x_bytes + y_bytes
+        
+        def encode_padding(size):
+            """
+            Encode whether or not we use left | top padding
+            """
+            x, y = size
+            x_pad, y_pad = 0, 0
+            if x % 2 != 0:
+                x_pad = 1
+            if y % 2 != 0:
+                y_pad = 1
+            return x_pad + 2*y_pad, x_pad, y_pad
             
         # initialize quadtree stream with the data describing the size of the map
-        encoded_dims = size_to_bytes(map.shape)
-        quadtree_stream = encoded_dims
+        encoded_dims = encode_size(map.shape)
+        encoded_pad, x_pad, y_pad = encode_padding(map.shape)
+        quadtree_stream = encoded_dims + [encoded_pad]
 
-        stack = [map]
+        # pad left and top with one row of unknown
+        padded_map = map
+        if x_pad:
+            padded_map = np.pad(padded_map, ((1, 0), (0, 0)), mode="constant", constant_values=UNKNOWN)
+        if y_pad:
+            padded_map = np.pad(padded_map, ((0, 0), (1, 0)), mode="constant", constant_values=UNKNOWN)
+
+        stack = [padded_map]
         while (len(stack) > 0):
             curr = stack.pop(0)
-            if len(np.unique(curr)) > 1:
-                stack += split(curr)
+            cells = np.unique(curr)
+            if len(cells) > 1:
+                stack = split(curr) + stack  # prepend
                 quadtree_stream.append(SPLIT_IND)
+            elif len(cells) == 0:
+                quadtree_stream.append(UNKNOWN_IND)
             else:
-                cell = np.unique(curr)
-                if cell == UNKNOWN:
+                if cells == UNKNOWN:
                     quadtree_stream.append(UNKNOWN_IND)
-                elif cell == FREE:
+                elif cells == FREE:
                     quadtree_stream.append(FREE_IND)
                 else:
                     quadtree_stream.append(OCCUPIED_IND)
@@ -113,28 +136,44 @@ class QuadTreeMap(Map):
                 y += v
             return x, y
         
+        def decode_padding(quadtree_arr):
+            padding = quadtree_arr[0]
+            x_pad, y_pad = 0, 0
+            if padding == 1:
+                x_pad = 1
+            elif padding == 2:
+                y_pad = 1
+            elif padding == 3:
+                x_pad = 1
+                y_pad = 1
+            return x_pad, y_pad
+        
         # take shape from head of quadtree
         shape = decode_size(quadtree_arr)
         quadtree_arr = quadtree_arr[8:] 
+        # decode padding if needed (for post processing)
+        x_pad, y_pad = decode_padding(quadtree_arr)
+        quadtree_arr = quadtree_arr[1:]
         
         # array to load values into
-        decoded = np.zeros(shape=shape, dtype=np.uint8)
+        decoded = np.zeros(shape=(shape[0]+x_pad, shape[1]+y_pad), dtype=np.uint8)
 
         # keep track of where we will be putting in values
-        offset_stack = [(0, 0, shape[0], shape[1])]
+        offset_stack = [(0, 0, shape[0]+x_pad, shape[1]+y_pad)]
         for val in quadtree_arr:
             start_x, start_y, end_x, end_y = offset_stack.pop(0)
             if val != SPLIT_IND:
                 decoded[start_x:end_x, start_y:end_y] = IND_MAP[val]
             else:
                 # pre-pend new offset indices for four splits
-                # TODO deal with non-square arrays via padding or other measures
                 mid_x, mid_y = (end_x + start_x) // 2, (end_y + start_y) // 2
                 q1_coord = (start_x, start_y, mid_x,  mid_y)
                 q2_coord = (start_x, mid_y, mid_x, end_y)
                 q3_coord = (mid_x, start_y, end_x, mid_y)
                 q4_coord = (mid_x, mid_y, end_x, end_y)
                 offset_stack = [q1_coord, q2_coord, q3_coord, q4_coord] + offset_stack
+        # handle padding
+        decoded = decoded[x_pad:, y_pad:,]
         return decoded
         
     def to_bytes(self) -> bytes:
@@ -143,17 +182,18 @@ class QuadTreeMap(Map):
     def from_bytes(self, data) -> bytes:
         qt = np.frombuffer(data, dtype=np.uint8)
         m = QuadTreeMap.decode_quadtree(qt)
-        m = m.reshape(self.num_rows, self.num_col)
         return Map(m)
 
 if __name__ == "__main__":
-    # basic sanity check using random map, make sure we dont lose any information
-    rand_map = np.random.uniform(low=0, high=1, size=(256, 256))
-    rand_map = np.where(rand_map <= 0.33, OCCUPIED, rand_map)
-    rand_map = np.where((rand_map > 0.33)&(rand_map <= 0.66), UNKNOWN, rand_map)
-    rand_map = np.where((rand_map > 0.66)&(rand_map <= 1.00), FREE, rand_map)
-    rand_map = rand_map.astype(np.uint8)
-    qt = QuadTreeMap(rand_map)
-    enc = qt.to_bytes()
-    dec = qt.from_bytes(enc)
-    # check rand_map vs. dec.map
+    # basic sanity check using random maps, make sure we dont lose any information
+    for size in [(128, 128), (200, 300), (201, 201), (201, 200), (200, 201)]:
+        rand_map = np.random.uniform(low=0, high=1, size=(size))
+        rand_map = np.where((rand_map > 0.33)&(rand_map <= 0.66), UNKNOWN, rand_map)
+        rand_map = np.where((rand_map > 0.66)&(rand_map <= 1.00), FREE, rand_map)
+        rand_map = np.where(rand_map <= 0.33, OCCUPIED, rand_map)
+        rand_map = rand_map.astype(np.uint8)
+        qt = QuadTreeMap(rand_map)
+        enc = qt.to_bytes()
+        dec = qt.from_bytes(enc)
+        print(f"Difference between decoded maps (size={size})", np.sum(dec.map != rand_map))
+        assert np.sum(dec.map != rand_map) == 0, "maps should not differ"
