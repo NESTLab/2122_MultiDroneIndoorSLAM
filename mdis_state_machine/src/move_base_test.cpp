8c6b310de4968681ddc3f86a0c31b0e79054e25b
Ashay Aswale <asaswale@wpi.edu>
2021-12-04 02:57:13 -0500

    move base interface for the state machine

---
+++ mdis_state_machine/src/move_base_test.cpp
@@ -0,0 +1,18 @@
+#include <move_base_interface.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "move_base_test");
+  ros::NodeHandle nh;
+  MoveBaseInterface explore(nh);
+  // ros::Duration(2).sleep();
+  geometry_msgs::Point point_1, point_2;
+  point_1.x = -31;
+  point_1.y = -8;
+  point_2.x = -31;
+  point_2.y = -6;
+  float dist = explore.getDistancePrediction(point_1, point_2);
+  ROS_INFO_STREAM("Distance: "<<dist);
+
+  return 0;
+}
