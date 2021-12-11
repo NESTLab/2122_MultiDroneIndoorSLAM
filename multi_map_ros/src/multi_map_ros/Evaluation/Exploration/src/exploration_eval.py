672a6eb001c779b72a78b37665aa82517a8dcd07
Taylor Bergeron <tberg1234@gmail.com>
2021-12-11 16:39:31 -0500

    [CHANGE] consistant timing for both percent and distance - can match up timestamps

--- multi_map_ros/src/multi_map_ros/Evaluation/Exploration/src/exploration_eval.py
+++ multi_map_ros/src/multi_map_ros/Evaluation/Exploration/src/exploration_eval.py
@@ -18,6 +18,7 @@ class exploration_eval:
         self.mapSub = rospy.Subscriber(self.robot_namespace+'/map', OccupancyGrid, self.map_callback)
         self.odomSub = rospy.Subscriber(self.robot_namespace+'/odom', Odometry, self.odom_callback)
         # self.odomPub = rospy.Publisher('/distance_travelled', Odometry, queue_size=10)
+        self.timer = rospy.Timer(rospy.Duration(5), self.timer_callback)
 
         self.start_time = rospy.get_time() #rospy.get_rostime()
 
@@ -32,44 +33,84 @@ class exploration_eval:
         self.percent_of_map_covered_time = []
         self.total_distance_travelled_time = []
 
+        self.map = OccupancyGrid()
+        self.odom = Odometry()
+
 
     def map_callback(self, msg):
-        data = np.asarray(msg.data)
-        values = [0, 101]
-        freq = np.histogram(data, bins=[-np.inf] + values + [np.inf])[0] / data.size
-        # print(freq)
 
-        # uniques, counts = np.unique(data, return_counts=True)
-        # percentages = dict(zip(uniques, counts / len(data)))
-        # print(percentages)
+        self.map = msg
 
-        # total_percent_explored = percentages.get(1) + percentages.get(0)
-
-        total_percent_explored = freq[1]
-
-        print("total Percent explored: " + str(total_percent_explored))
-
-        self.percent_of_map_covered_record.append(total_percent_explored)
-        self.percent_of_map_covered_time.append(rospy.get_time() - self.start_time)
+        # data = np.asarray(msg.data)
+        # values = [0, 101]
+        # freq = np.histogram(data, bins=[-np.inf] + values + [np.inf])[0] / data.size
+        # # print(freq)
+        #
+        # # uniques, counts = np.unique(data, return_counts=True)
+        # # percentages = dict(zip(uniques, counts / len(data)))
+        # # print(percentages)
+        #
+        # # total_percent_explored = percentages.get(1) + percentages.get(0)
+        #
+        # total_percent_explored = freq[1]
+        #
+        # print("total Percent explored: " + str(total_percent_explored))
+        #
+        # self.percent_of_map_covered_record.append(total_percent_explored)
+        # self.percent_of_map_covered_time.append(rospy.get_time() - self.start_time)
 
 
     def odom_callback(self, msg):
+
+        self.odom = msg
+
+        # if (self.first_run):
+        #     self.previous_x = msg.pose.pose.position.x
+        #     self.previous_y = msg.pose.pose.position.y
+        # x = msg.pose.pose.position.x
+        # y = msg.pose.pose.position.y
+        # print(msg)
+        # d_increment = math.sqrt(((x - self.previous_x) * (x - self.previous_x)) + ((y - self.previous_y) * (y - self.previous_y)))
+        # self.total_distance = self.total_distance + d_increment
+        # print("Total distance traveled is: " + str(self.total_distance))
+        # # self.odomPub.publish(data)
+        # self.previous_x = msg.pose.pose.position.x
+        # self.previous_y = msg.pose.pose.position.y
+        # self.first_run = False
+        #
+        # self.total_distance_travelled_record.append(self.total_distance)
+        # self.total_distance_travelled_time.append(rospy.get_time() - self.start_time)
+
+
+    def timer_callback(self, timer_info):
+
+        data = np.asarray(self.map.data)
+        odom = self.odom
+
+        values = [0, 101]
+        freq = np.histogram(data, bins=[-np.inf] + values + [np.inf])[0] / data.size
+
+        total_percent_explored = freq[1]
+
+        print("Total Percent explored: " + str(total_percent_explored))
+
+        self.percent_of_map_covered_record.append(total_percent_explored)
+
         if (self.first_run):
-            self.previous_x = msg.pose.pose.position.x
-            self.previous_y = msg.pose.pose.position.y
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-        print(msg)
+            self.previous_x = odom.pose.pose.position.x
+            self.previous_y = odom.pose.pose.position.y
+        x = odom.pose.pose.position.x
+        y = odom.pose.pose.position.y
         d_increment = math.sqrt(((x - self.previous_x) * (x - self.previous_x)) + ((y - self.previous_y) * (y - self.previous_y)))
         self.total_distance = self.total_distance + d_increment
         print("Total distance traveled is: " + str(self.total_distance))
-        # self.odomPub.publish(data)
-        self.previous_x = msg.pose.pose.position.x
-        self.previous_y = msg.pose.pose.position.y
+        self.previous_x = odom.pose.pose.position.x
+        self.previous_y = odom.pose.pose.position.y
         self.first_run = False
 
         self.total_distance_travelled_record.append(self.total_distance)
-        self.total_distance_travelled_time.append(rospy.get_time() - self.start_time)
+
+        self.time_record.append(rospy.get_time() - self.start_time)
 
 
 
