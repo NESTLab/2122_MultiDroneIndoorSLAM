33461f521770746246fd010a00f23306f51a9537
Connor Mclaughlin <cjmclaughlin@wpi.edu>
2022-05-10 10:30:55 -0400

    log all maps

--- coms/coms/src/mapmerge/service.py
+++ coms/coms/src/mapmerge/service.py
@@ -55,9 +55,10 @@ def mapmerge_pipeline(map1, map2, method="hough", scale_process=False, median_pr
             transformed_map2, M, acpt = merge_fn(map1, map2)
         #transformed_map2 = apply_warp(map2, M)
         # log errors
-        if acpt < 0.9:
-            cv2.imwrite(f"map1_acpt_{acpt}_time_{datetime.now()}.png", map1)
-            cv2.imwrite(f"map2_acpt_{acpt}_time_{datetime.now()}.png", map2)
-            cv2.imwrite(f"aligned_acpt_{acpt}_time_{datetime.now()}.png", transformed_map2)
+        # if acpt < 0.95:
+        #     return map1
+        cv2.imwrite(f"map1_acpt_{acpt}_time_{datetime.now()}.png", map1)
+        cv2.imwrite(f"map2_acpt_{acpt}_time_{datetime.now()}.png", map2)
+        cv2.imwrite(f"aligned_acpt_{acpt}_time_{datetime.now()}.png", transformed_map2)
         merged_map = combine_aligned_maps(transformed_map2, map1)
         return merged_map
